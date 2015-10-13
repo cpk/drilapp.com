38def6b6e696052c06d9b4a07178320acd66fbab
peterjurkovic <petojurkovic@gmail.com>
2015-10-13 21:22:49 +0100

    bug fix - delete all words in the lectur

--- api/controllers/LectureController.php
+++ api/controllers/LectureController.php
@@ -24,7 +24,7 @@ class LectureController{
      * @url PUT /v1/lectures
      */
     public function update( $data, $uid )
-    {   
+    {
         $book = $this->bookService->getBookById( $data->dril_book_id );
         checkBookPermision($book, $uid);
         return $this->lectureService->update( $data );
@@ -35,14 +35,14 @@ class LectureController{
      * Activate user word
      *
      * @url DELETE /v1/user/lectures/$id/words
-     * 
+     *
      */
     public function deleteLectureWords($id, $uid){
         global $conn;
         $wordService = new WordService($conn);
         $book = $wordService->getBookByLectureId( $id );
         checkBookPermision($book, $uid);
-        $wordService->deleteWordsOnly( $id );
+        $this->lectureService->deleteWordsOnly( $id );
         $wordService->updateCountOfWordsByLectureId( $id );
     }
 
@@ -51,7 +51,7 @@ class LectureController{
      * Activate user word
      *
      * @url DELETE /v1/user/lectures/$id
-     * 
+     *
      */
     public function deleteLecture($id, $uid){
         global $conn;
@@ -61,11 +61,11 @@ class LectureController{
         $this->lectureService->delete( $id );
     }
 
-    
+
     public function init(){
         global $conn;
         $this->lectureService = new LectureService($conn);
         $this->bookService = new BookService($conn, $this->lectureService);
     }
-    
+
 }
